c1c863579ddd0aa250e620543432ffca668aa99e
Govind Kumar Jat <govindjatkumar@gmail.com>
2025-04-03 15:26:16 +0530

    calc.java addition feature

---
+++ calc.java
@@ -0,0 +1,9 @@
+public class Calc{
+	public static void main(String[] args){
+		int a;
+		int b;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two numbers to add (a,b):");
+		a=sc.nextInt();
+		b=sc.nextInt();
+		System.out.println("Sum of a and b is :"+(a+b));
